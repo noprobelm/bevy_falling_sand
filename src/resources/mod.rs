17f5832b189e810744c5adf160f8a6debf0cb52a
noprobelm <noprobelm@protonmail.com>
2024-06-16 19:22:17 -0400

    Added resources module and SimulationPause/SimulationBatch resources

---
+++ src/resources/mod.rs
@@ -0,0 +1,3 @@
+pub use simulation::*;
+
+mod simulation;
---
+++ src/resources/simulation.rs
@@ -0,0 +1,9 @@
+use bevy::prelude::Resource;
+
+/// Resource to insert for pausing the simulation
+#[derive(Resource)]
+pub struct SimulationPause;
+
+/// Resource to insert for parallel queries and batching
+#derive(Resource)
+pub struct SimulationBatch;
