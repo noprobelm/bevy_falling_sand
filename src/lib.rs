908e52badca36417800f3612ae4ba2316b8fb68b
noprobelm <noprobelm@protonmail.com>
2024-06-16 19:19:24 -0400

    First commit

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+//! `bevy_falling_sand` is a generic plugin for adding falling sand simulation physics to your Bevy project.
+
+use bevy::prelude::*;
+
+pub struct FallingSandPlugin;
+
+impl Plugin for FallingSandPlugin {
+    fn build(&self, app: &mut App) {
+    }
+}
