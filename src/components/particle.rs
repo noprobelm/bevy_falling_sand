713d66895b446ade20d7ecaea335a85aee07282d
noprobelm <noprobelm@protonmail.com>
2024-06-16 20:01:31 -0400

    Added ParticleParent marker component

--- src/components/particle.rs
+++ src/components/particle.rs
@@ -4,6 +4,9 @@ use bevy::prelude::*;
 #[derive(Component, Reflect, Debug, Default)]
 pub struct Particle;
 
+/// Marker component for particle parent entities.
+pub struct ParticleParent;
+
 /// This component manages a particle's velocity
 #[derive(Component, Reflect, Debug, Default)]
 pub struct Velocity {
@@ -43,8 +46,10 @@ pub struct Density(pub u32);
 /// A sequence of possible neighbors for a particle to consider as part of its movement logic.
 /// The inner vectors are neighbors that should be considered as equal candidates when assessing
 /// where a particle should attempt to relocate to. The order of the inner vectors can be thought
-/// of as *priority*
+/// of as *prioritized tier* whe considering groups of movement candidates. For example, a sand
+/// particle's order of movement might look like [[0, -1], [1, -1, -1, -1]]. The particle would
+/// first attempt to move directly downward. If this fails, it would then look to its lower left
+/// and lower right neighbors at random, considering each as equally prioritized candidates.
 #[derive(Component, Debug, Default, Reflect)]
 #[reflect(Component)]
 pub struct Neighbors(pub Vec<Vec<IVec2>>);
-
