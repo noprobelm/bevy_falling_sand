a890eb13ed0d06fb27187da69b05b5763afd1ebd
noprobelm <noprobelm@protonmail.com>
2024-06-22 10:22:45 -0400

    Added allownace for dead code to PhysicsRng component

--- src/components/rng.rs
+++ src/components/rng.rs
@@ -2,5 +2,6 @@ use bevy::prelude::Component;
 use bevy_turborand::prelude::RngComponent;
 
 /// The physics rng to use when dealing with any entity that needs random movement behaviors.
+#[allow(dead_code)]
 #[derive(Component, Default)]
 pub struct PhysicsRng(RngComponent);
