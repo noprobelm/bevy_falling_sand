1c042c97bd30debcf72316c5d5b31b69384f8936
noprobelm <noprobelm@protonmail.com>
2024-06-22 10:26:13 -0400

    Moved hibernation components to their own module

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -2,8 +2,10 @@ pub use particle_types::*;
 pub use particle::*;
 pub use rng::*;
 pub use color::*;
+pub use hibernation::*;
 
 mod particle_types;
 mod particle;
 mod rng;
 mod color;
+mod hibernation;
