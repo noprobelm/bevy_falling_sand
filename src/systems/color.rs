d96897145b97090611fc31f71fd31cf4b357d995
noprobelm <noprobelm@protonmail.com>
2024-07-10 22:56:49 -0400

    Removed old debugging code

--- src/systems/color.rs
+++ src/systems/color.rs
@@ -3,21 +3,6 @@ use crate::{
 };
 use bevy::prelude::*;
 
-#[allow(dead_code)]
-pub fn color_particles_debug(
-    mut particle_query: Query<(&mut Sprite, &ParticleColor, Option<&Sleeping>)>,
-) {
-    particle_query
-        .par_iter_mut()
-        .for_each(|(mut sprite, color, hibernating)| {
-            if hibernating.is_some() {
-                sprite.color = Color::RED;
-            } else {
-                sprite.color = color.0;
-            }
-        });
-}
-
 pub fn color_particles(
     mut particle_query: Query<
         (&mut Sprite, &ParticleColor),
