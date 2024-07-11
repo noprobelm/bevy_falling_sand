39635544aa8f65df41ddcecce4aa66e26766fbca
noprobelm <noprobelm@protonmail.com>
2024-07-10 22:56:33 -0400

    Added debug module

---
+++ src/systems/debug.rs
@@ -0,0 +1,23 @@
+use crate::*;
+use bevy::prelude::*;
+
+pub fn color_chunks(map: Res<ParticleMap>, mut chunk_gizmos: Gizmos<ChunkGizmos>) {
+    map.iter_chunks().for_each(|chunk| {
+        let rect = Rect::from_corners(chunk.min.as_vec2(), chunk.max.as_vec2());
+        if chunk.sleeping == true {
+            chunk_gizmos.rect_2d(rect.center(), 0., rect.size() + Vec2::splat(1.), Color::rgba(0.67, 0.21, 0.24, 1.));
+        }
+    });
+
+    map.iter_chunks().for_each(|chunk| {
+        let rect = Rect::from_corners(chunk.min.as_vec2(), chunk.max.as_vec2());
+        if chunk.sleeping == false {
+            chunk_gizmos.rect_2d(
+                rect.center(),
+                0.,
+                rect.size() + Vec2::splat(1.),
+                Color::rgba(0.52,0.80,0.51, 1.0),
+            );
+        }
+    });
+}
