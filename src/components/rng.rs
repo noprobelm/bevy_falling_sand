7076d254e03ee95bce094eed35d285b7b230c4c5
noprobelm <noprobelm@protonmail.com>
2024-07-11 22:21:03 -0400

    Updated docs for rng

--- src/components/rng.rs
+++ src/components/rng.rs
@@ -2,7 +2,7 @@ use bevy::prelude::Component;
 use bevy_turborand::DelegatedRng;
 use bevy_turborand::prelude::RngComponent;
 
-/// The physics rng to use when dealing with any entity that needs random movement behaviors.
+/// RNG to use when dealing with any entity that needs random movement behaviors.
 #[allow(dead_code)]
 #[derive(Component, Default)]
 pub struct PhysicsRng(RngComponent);
