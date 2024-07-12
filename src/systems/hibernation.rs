aa31c52c033b91bd4434fa112e5187e55aa46085
noprobelm <noprobelm@protonmail.com>
2024-07-11 21:42:45 -0400

    Removed unused 'hibernate_particles' system from hibernation mod

--- src/systems/hibernation.rs
+++ src/systems/hibernation.rs
@@ -7,22 +7,3 @@ pub fn reset_chunks(
     map.sleep_chunks(commands);
     map.deactivate_all_chunks();
 }
-
-pub fn hibernate_particles(
-    par_commands: ParallelCommands,
-    particle_query: Query<(Entity, &LastMoved, Option<&Hibernating>)>,
-) {
-    particle_query
-        .par_iter()
-        .for_each(|(entity, last_moved, hibernating)| {
-            if hibernating.is_none() && last_moved.0.elapsed().as_millis() > 300 {
-                par_commands.command_scope(|mut commands| {
-                    commands.entity(entity).insert(Hibernating::default());
-                });
-            } else if hibernating.is_some() && last_moved.0.elapsed().as_millis() < 300 {
-                par_commands.command_scope(|mut commands| {
-                    commands.entity(entity).remove::<Hibernating>();
-                });
-            }
-        });
-}
