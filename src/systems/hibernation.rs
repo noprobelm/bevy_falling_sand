4c3cffc0d9db9f49d12ccde70708d5c1a1c8faf2
noprobelm <noprobelm@protonmail.com>
2024-07-12 12:22:43 -0400

    Docs

--- src/systems/hibernation.rs
+++ src/systems/hibernation.rs
@@ -1,5 +1,10 @@
 use crate::*;
 
+/// Resets all chunks in preparation for the next frame
+///
+/// When this system runs, all chunks are checked to see if they should be awakened in preparation for the next frame
+/// (see field `should_process_this_frame`). After this, their 'activated' status is reset (see field
+/// `should_process_next_frame`)
 pub fn reset_chunks(
     commands: Commands,
     mut map: ResMut<ChunkMap>,
