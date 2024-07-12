c417f7162618bd991dd9dba3871c2b7bdb39cb5c
noprobelm <noprobelm@protonmail.com>
2024-07-11 22:06:20 -0400

    Updated docs for components/particle module

--- src/components/particle.rs
+++ src/components/particle.rs
@@ -4,15 +4,13 @@ use bevy::prelude::*;
 #[derive(Component, Reflect, Debug, Default)]
 pub struct Particle;
 
-/// Marker component for particle parent entities. Parent particle entities are generally responsible
-/// for holding common component data between particles, such as density or neighbor priority
-/// selections.
+/// Marker component for particle parent entities. Parent particle entities are generally responsible for holding
+/// common component data between particles, such as density or neighbor priority selections.
 #[derive(Component, Reflect, Debug, Default)]
 pub struct ParticleParent;
 
-/// This component keeps track of the coordinate of a given particle. This is primarily used for
-/// movement detection from one frame to the next, so we can do things like track time last moved
-/// (for hibernation)
+/// Keeps track of the coordinate of a given particle. This is primarily used for movement detection from one frame to
+/// the next, so we can do things like track time last moved (for hibernation)
 #[derive(Component, Reflect, Clone, Default, Debug, Eq, PartialEq)]
 #[reflect(Component)]
 pub struct Coordinates(pub IVec2);
@@ -22,28 +20,25 @@ pub struct Coordinates(pub IVec2);
 #[reflect(Component, Debug)]
 pub struct Density(pub u32);
 
-/// A sequence of possible neighbors for a particle to consider as part of its movement logic.
-/// The inner vectors are neighbors that should be considered as equal candidates when assessing
-/// where a particle should attempt to relocate to. The order of the inner vectors can be thought
-/// of as *prioritized tier* whe considering groups of movement candidates.
-///
-/// For example, a sand particle's order of movement might look like
-/// `[[[0, -1]], [[1, -1], [-1, -1]]]`. The particle would first attempt to move directly downward.
-/// If this fails, it would then look to its lower left and lower right neighbors at random,
-/// considering each as equally prioritized candidates.
+/// A sequence of possible neighbors for a particle to consider as part of its movement logic. The inner vectors are
+/// neighbors that should be considered as equal candidates when assessing where a particle should attempt to relocate
+/// to. The order of the inner vectors can be thought of as *prioritized tier* whe considering groups of movement
+/// candidates.
+/// For example, a sand particle's order of movement might look like `[[[0, -1]], [[1, -1], [-1, -1]]]`. The particle
+/// would first attempt to move directly downward. If this fails, it would then look to its lower left and lower right
+/// neighbors at random, considering each as equally prioritized candidates.
 #[derive(Component, Reflect, Debug, Default)]
 #[reflect(Component)]
 pub struct Neighbors(pub Vec<Vec<IVec2>>);
 
-/// This component indicates whether a particle should be considered for movement or not. Examples
-/// of anchored particles might be the ground, or walls. We want anchored particles to be considered
-/// as impenetrable neighbors (excluding their ability to be destroyed), but without uninstigated
-/// movement.
+/// Indicates whether a particle should be considered for movement or not. Examples of anchored particles might be the
+/// ground, or walls. We want anchored particles to be considered as impenetrable neighbors (excluding their ability to
+/// be destroyed), but without uninstigated movement.
 #[derive(Component, Debug, Default, Reflect)]
 #[reflect(Component)]
 pub struct Anchored;
 
-/// This component manages a particle's velocity
+/// Manages a particle's velocity
 #[derive(Component, Reflect, Debug, Default)]
 pub struct Velocity {
     /// The current velocity of the particle
@@ -77,20 +72,18 @@ impl Velocity {
 
 }
 
-/// This component provides optional momentum for particles. If a particle possesses the ability to
-/// obtain momentum (as indicated by the presence of this component on its parent type), it will
-/// attempt to relocate itself to the relative coordinate indicated by the IVec2 field *if* the
-/// coordinate resides within the inner vector currently being considered in the Neighbors
-/// selection strategy.
+/// Provides optional momentum for particles. If a particle possesses the ability to obtain momentum (as indicated by
+/// the presence of this component on its parent type), it will attempt to relocate itself to the relative coordinate
+/// indicated by the IVec2 field *if* the coordinate resides within the inner vector currently being considered in the
+/// Neighbors selection strategy.
 ///
-/// The value of the IVec2 field is influenced by the successful, unobstructed movement of the
-/// particle as part of its previous step.
+/// The value of the IVec2 field is influenced by the successful, unobstructed movement of the particle as part of its
+/// previous step.
 #[derive(Component, Reflect, Clone, Default, Debug)]
 #[reflect(Component)]
 pub struct Momentum(pub IVec2);
 
 impl Momentum {
-    /// Use this value for momentum if the particle is capable of gaining momentum, but currently
-    /// has none.
+    /// Used if the particle is capable of gaining momentum, but currently has none.
     pub const ZERO: Self = Self(IVec2::splat(0));
 }
