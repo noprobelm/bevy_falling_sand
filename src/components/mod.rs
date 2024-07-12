dcbef7f91b9015998cacc5fba5929cc8385804e4
noprobelm <noprobelm@protonmail.com>
2024-07-11 21:43:22 -0400

    Removed chunks module and its references

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -3,11 +3,9 @@ pub use particle::*;
 pub use rng::*;
 pub use color::*;
 pub use hibernation::*;
-pub use chunks::*;
 
 mod particle_types;
 mod particle;
 mod rng;
 mod color;
 mod hibernation;
-mod chunks;
