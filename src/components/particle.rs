901b558533bb067bc4646d4cc6420e967b71355a
noprobelm <noprobelm@protonmail.com>
2024-07-13 14:05:23 -0400

    Added common traits

--- src/components/particle.rs
+++ src/components/particle.rs
@@ -1,17 +1,19 @@
 use bevy::prelude::*;
 
 /// Marker component for querying for particles
-#[derive(Component, Reflect, Debug, Default)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default, Component, Reflect)]
+#[reflect(Component)]
 pub struct Particle;
 
 /// Marker component for particle parent entities. Parent particle entities are generally responsible for holding
 /// common component data between particles, such as density or neighbor priority selections.
-#[derive(Component, Reflect, Debug, Default)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default, Component, Reflect)]
+#[reflect(Component)]
 pub struct ParticleParent;
 
 /// Keeps track of the coordinate of a given particle. This is primarily used for movement detection from one frame to
 /// the next, so we can do things like track time last moved (for hibernation)
-#[derive(Component, Reflect, Clone, Default, Debug, Eq, PartialEq)]
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug, Default, Component, Reflect)]
 #[reflect(Component)]
 pub struct Coordinates(pub IVec2);
 
@@ -27,19 +29,20 @@ pub struct Density(pub u32);
 /// For example, a sand particle's order of movement might look like `[[[0, -1]], [[1, -1], [-1, -1]]]`. The particle
 /// would first attempt to move directly downward. If this fails, it would then look to its lower left and lower right
 /// neighbors at random, considering each as equally prioritized candidates.
-#[derive(Component, Reflect, Debug, Default)]
+#[derive(Clone, Eq, PartialEq, Hash, Debug, Default, Component, Reflect)]
 #[reflect(Component)]
 pub struct Neighbors(pub Vec<Vec<IVec2>>);
 
 /// Indicates whether a particle should be considered for movement or not. Examples of anchored particles might be the
 /// ground, or walls. We want anchored particles to be considered as impenetrable neighbors (excluding their ability to
 /// be destroyed), but without uninstigated movement.
-#[derive(Component, Debug, Default, Reflect)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default, Component, Reflect)]
 #[reflect(Component)]
 pub struct Anchored;
 
 /// Manages a particle's velocity
-#[derive(Component, Reflect, Debug, Default)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default, Component, Reflect)]
+#[reflect(Component)]
 pub struct Velocity {
     /// The current velocity of the particle
     pub val: u8,
@@ -79,7 +82,7 @@ impl Velocity {
 ///
 /// The value of the IVec2 field is influenced by the successful, unobstructed movement of the particle as part of its
 /// previous step.
-#[derive(Component, Reflect, Clone, Default, Debug)]
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug, Default, Component, Reflect)]
 #[reflect(Component)]
 pub struct Momentum(pub IVec2);
 
