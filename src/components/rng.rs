ee931c84fa583b55b65820d9c0f037c7c93961e6
noprobelm <noprobelm@protonmail.com>
2024-07-13 14:03:46 -0400

    Removed dead code allowance

--- src/components/rng.rs
+++ src/components/rng.rs
@@ -1,10 +1,9 @@
-use bevy::prelude::{Reflect, Component};
+use bevy::prelude::Component;
 use bevy_turborand::DelegatedRng;
 use bevy_turborand::prelude::RngComponent;
 
 /// RNG to use when dealing with any entity that needs random movement behaviors.
-#[derive(Clone, PartialEq, Debug, Default, Component, Reflect)]
-#[reflect(Component)]
+#[derive(Clone, PartialEq, Debug, Default, Component)]
 pub struct PhysicsRng(RngComponent);
 
 impl PhysicsRng {
