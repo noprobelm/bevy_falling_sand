fedc46a9a18cc95a64d92f61e9904a9a82061bb3
noprobelm <noprobelm@protonmail.com>
2024-07-13 14:04:36 -0400

    Added Reflect for PhysicsRng

--- src/components/rng.rs
+++ src/components/rng.rs
@@ -1,9 +1,10 @@
-use bevy::prelude::Component;
+use bevy::prelude::*;
 use bevy_turborand::DelegatedRng;
 use bevy_turborand::prelude::RngComponent;
 
 /// RNG to use when dealing with any entity that needs random movement behaviors.
-#[derive(Clone, PartialEq, Debug, Default, Component)]
+#[derive(Clone, PartialEq, Debug, Default, Component, Reflect)]
+#[reflect(Component)]
 pub struct PhysicsRng(RngComponent);
 
 impl PhysicsRng {
