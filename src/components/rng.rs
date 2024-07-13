5d2255dc1159264e562452ca4aba8fb0d185e0ab
noprobelm <noprobelm@protonmail.com>
2024-07-13 18:44:56 -0400

    Docs

--- src/components/rng.rs
+++ src/components/rng.rs
@@ -1,3 +1,5 @@
+//! RNG used for particle movement.[^note]
+//! [^note]: It might be better to move this to Chunks in the future. This would eliminate the need of having potentially hundreds of thousands of RNG's for every particle in the world
 use bevy::prelude::*;
 use bevy_turborand::DelegatedRng;
 use bevy_turborand::prelude::RngComponent;
