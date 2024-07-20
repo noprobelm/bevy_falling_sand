557e145948ee83e4e39f88a8ad277a6b65e9fb06
noprobelm <noprobelm@protonmail.com>
2024-07-20 16:07:30 -0400

    Added SmallVec to replace neighbor movement priority Vec<Vec<IVec2>>

--- src/components/movement_priority.rs
+++ src/components/movement_priority.rs
@@ -1,15 +1,16 @@
 use bevy::prelude::*;
+use smallvec::{smallvec, SmallVec};
 
-/// A particle's neighbors, represented as a nested vector of IVec2. The outer vectors represent tiered "groups" of
+/// A particle's neighbors, represented as a nested SmallVec of IVec2. The outer vectors represent tiered "groups" of
 /// priority, whereas the inner vectors are representative of relative coordinates a particle might move to. See
 /// the [handle_particles](crate::handle_particles) system.
 #[derive(Clone, Eq, PartialEq, Hash, Debug, Default, Component, Reflect)]
 #[reflect(Component)]
-pub struct MovementPriority(pub Vec<Vec<IVec2>>);
+pub struct MovementPriority(pub SmallVec<[SmallVec<[IVec2; 4]>; 8]>);
 
 pub trait Material {
     fn into_movement_priority(&self) -> MovementPriority {
-	MovementPriority(Vec::new())
+        MovementPriority(SmallVec::new())
     }
 }
 
@@ -17,82 +18,104 @@ pub struct Wall;
 
 impl Wall {
     pub fn new() -> Wall {
-	Wall
+        Wall
     }
 }
 
 impl Material for Wall {}
 
+pub struct Solid;
+
+impl Solid {
+    pub fn new() -> Solid {
+        Solid
+    }
+}
+
+impl Material for Solid {
+    fn into_movement_priority(&self) -> MovementPriority {
+        MovementPriority(
+            smallvec![
+                smallvec![IVec2::NEG_Y],
+            ]
+        )
+    }
+}
+
 pub struct MovableSolid;
+
 
 impl MovableSolid {
     pub fn new() -> MovableSolid {
-	MovableSolid
+        MovableSolid
     }
 }
 
 impl Material for MovableSolid {
-     fn into_movement_priority(&self) -> MovementPriority {
-        MovementPriority(vec![
-            vec![IVec2::NEG_Y],
-            vec![IVec2::NEG_ONE, IVec2::new(1, -1)],
-        ])
+    fn into_movement_priority(&self) -> MovementPriority {
+        MovementPriority(
+            smallvec![
+                smallvec![IVec2::NEG_Y],
+                smallvec![IVec2::NEG_ONE, IVec2::new(1, -1)],
+            ]
+        )
     }
 }
 
 pub struct Liquid {
-    fluidity: usize
+    fluidity: usize,
 }
 
 impl Liquid {
     pub fn new(fluidity: usize) -> Liquid {
-	if fluidity > 5 {
-	    return Liquid {fluidity: 5}
-	} else {
-	    return Liquid { fluidity }
-	}
+        if fluidity > 5 {
+            Liquid { fluidity: 5 }
+        } else {
+            Liquid { fluidity }
+        }
     }
 }
 
 impl Material for Liquid {
     fn into_movement_priority(&self) -> MovementPriority {
-	let mut neighbors: Vec<Vec<IVec2>> = vec![
-	    vec![IVec2::NEG_Y],
-	    vec![IVec2::NEG_ONE, IVec2::new(1, -1)],
-	    vec![IVec2::X, IVec2::NEG_X]];
+        let mut neighbors: SmallVec<[SmallVec<[IVec2; 4]>; 8]> = smallvec![
+            smallvec![IVec2::NEG_Y],
+            smallvec![IVec2::NEG_ONE, IVec2::new(1, -1)],
+            smallvec![IVec2::X, IVec2::NEG_X]
+        ];
 
-	for i in 0..self.fluidity {
-	    neighbors.push(vec![IVec2::X * (i + 2) as i32, IVec2::NEG_X * (i + 2) as i32])
-	}
+        for i in 0..self.fluidity {
+            neighbors.push(smallvec![IVec2::X * (i + 2) as i32, IVec2::NEG_X * (i + 2) as i32]);
+        }
 
-	MovementPriority(neighbors)
-    }}
+        MovementPriority(neighbors)
+    }
+}
 
 pub struct Gas {
-    fluidity: usize
+    fluidity: usize,
 }
 
 impl Gas {
     pub fn new(fluidity: usize) -> Gas {
-	if fluidity > 5 {
-	    return Gas {fluidity: 5}
-	} else {
-	    return Gas { fluidity }
-	}
+        if fluidity > 5 {
+            Gas { fluidity: 5 }
+        } else {
+            Gas { fluidity }
+        }
     }
-
-
 }
 
 impl Material for Gas {
     fn into_movement_priority(&self) -> MovementPriority {
-	let mut neighbors: Vec<Vec<IVec2>> = vec![
-	    vec![IVec2::Y, IVec2::new(1, 1), IVec2::new(-1, 1)]];
+        let mut neighbors: SmallVec<[SmallVec<[IVec2; 4]>; 8]> = smallvec![
+            smallvec![IVec2::Y, IVec2::new(1, 1), IVec2::new(-1, 1)]
+        ];
 
-	for i in 0..self.fluidity {
-	    neighbors.push(vec![IVec2::X * (i + 2) as i32, IVec2::NEG_X * (i + 2) as i32])
-	}
+        for i in 0..self.fluidity {
+            neighbors.push(smallvec![IVec2::X * (i + 2) as i32, IVec2::NEG_X * (i + 2) as i32]);
+        }
 
-	MovementPriority(neighbors)
+        MovementPriority(neighbors)
     }
 }
