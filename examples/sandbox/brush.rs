9f224c5f014d0da1546b6c1387567c1ee2239156
noprobelm <noprobelm@protonmail.com>
2024-09-09 19:08:12 -0400

    capsule_direction doesn't need to be calculated each loop.

--- examples/sandbox/brush.rs
+++ examples/sandbox/brush.rs
@@ -312,13 +312,13 @@ fn points_within_capsule(capsule: &Capsule2d, start: Vec2, end: Vec2) -> Vec<IVe
     let max_x = (start.x.max(end.x) + capsule.radius).ceil() as i32;
     let min_y = (start.y.min(end.y) - capsule.radius).floor() as i32;
     let max_y = (start.y.max(end.y) + capsule.radius).ceil() as i32;
+    let capsule_direction = (end - start).normalize();
 
     for x in min_x..=max_x {
         for y in min_y..=max_y {
             let point = Vec2::new(x as f32, y as f32);
 
             let to_point = point - start;
-            let capsule_direction = (end - start).normalize();
             let projected_length = to_point.dot(capsule_direction);
             let clamped_length = projected_length.clamp(-capsule.half_length, capsule.half_length);
 
