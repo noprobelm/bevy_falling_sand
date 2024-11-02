eb2c751a046920be3d591bf3b0c7f131258274ef
noprobelm <noprobelm@protonmail.com>
2024-11-02 00:20:13 -0400

    Docs

--- crates/bfs_core/src/chunk_map/chunk_map.rs
+++ crates/bfs_core/src/chunk_map/chunk_map.rs
@@ -370,10 +370,6 @@ impl Chunk {
 }
 
 /// Resets all chunks in preparation for the next frame
-///
-/// When this system runs, all chunks are checked to see if they should be awakened in preparation for the next frame
-/// (see field `should_process_this_frame`). After this, their 'activated' status is reset (see field
-/// `should_process_next_frame`)
 pub fn reset_chunks(mut map: ResMut<ChunkMap>) {
     map.reset_chunks();
 }
