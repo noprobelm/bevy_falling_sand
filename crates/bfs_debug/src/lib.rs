dcb03a165a7d356be94d56e453df6ff18f85bb05
noprobelm <noprobelm@protonmail.com>
2024-11-04 20:15:44 -0500

    Added chunk debug drawing logic

--- crates/bfs_debug/src/lib.rs
+++ crates/bfs_debug/src/lib.rs
@@ -46,6 +46,28 @@ pub fn color_chunks(map: Res<ChunkMap>, mut chunk_gizmos: Gizmos<DebugGizmos>) {
                 Color::srgba(1., 1., 1., 1.),
             )
         }
+
+        let rect = Rect::from_corners(chunk.min().as_vec2(), chunk.max().as_vec2());
+        if chunk.hibernating() == true {
+            chunk_gizmos.rect_2d(
+                rect.center(),
+                0.,
+                rect.size() + Vec2::splat(1.),
+                Color::srgba(0.67, 0.21, 0.24, 1.),
+            );
+        }
+    });
+
+    map.iter_chunks().for_each(|chunk| {
+        let rect = Rect::from_corners(chunk.min().as_vec2(), chunk.max().as_vec2());
+        if chunk.hibernating() == false {
+            chunk_gizmos.rect_2d(
+                rect.center(),
+                0.,
+                rect.size() + Vec2::splat(1.),
+                Color::srgba(0.52, 0.80, 0.51, 1.0),
+            );
+        }
     });
 }
 
