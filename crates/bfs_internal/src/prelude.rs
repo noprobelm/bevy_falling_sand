46572687ce99c171e9c48f396b5c35a3f9ad08b6
noprobelm <noprobelm@protonmail.com>
2025-01-01 17:29:45 -0500

    Added prelude.rs

---
+++ crates/bfs_internal/src/prelude.rs
@@ -0,0 +1,9 @@
+pub use crate::bundles::*;
+pub use crate::core::*;
+pub use crate::color::*;
+pub use crate::movement::*;
+pub use crate::debug::*;
+pub use crate::spatial::*;
+pub use crate::reactions::*;
+pub use crate::scenes::*;
+pub use super::FallingSandPlugin;
