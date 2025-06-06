d234e98a7244ae5d2d9c0957697db9176898f438
Jeff Barfield <jeffreybarfield@protonmail.com>
2025-06-06 01:13:42 -0400

    Replace deprecated method with new

--- crates/bfs_color/src/particle_definitions.rs
+++ crates/bfs_color/src/particle_definitions.rs
@@ -114,13 +114,13 @@ fn handle_particle_components(
         ),
         With<ParticleType>,
     >,
-    particle_query: &Query<&Parent, With<Particle>>,
+    particle_query: &Query<&ChildOf, With<Particle>>,
     entities: &Vec<Entity>,
 ) {
     entities.iter().for_each(|entity| {
-        if let Ok(parent) = particle_query.get(*entity) {
+        if let Ok(child_of) = particle_query.get(*entity) {
             commands.entity(*entity).insert(ColorRng::default());
-            if let Ok((particle_color, flows_color)) = parent_query.get(parent.get()) {
+            if let Ok((particle_color, flows_color)) = parent_query.get(child_of.parent()) {
                 commands.entity(*entity).insert((
                     Sprite {
                         color: Color::srgba(0., 0., 0., 0.),
@@ -156,7 +156,7 @@ fn handle_particle_registration(
         ),
         With<ParticleType>,
     >,
-    particle_query: Query<&Parent, With<Particle>>,
+    particle_query: Query<&ChildOf, With<Particle>>,
     mut ev_particle_registered: EventReader<ParticleRegistrationEvent>,
     mut ev_reset_particle_color: EventReader<ResetParticleColorEvent>,
 ) {
