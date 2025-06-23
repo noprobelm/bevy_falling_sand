5c36a2b9875a701f17b98ab18420f700d31d2018
Jeff Barfield <jeffreybarfield@protonmail.com>
2025-06-23 01:27:15 -0400

    Fix active chunk rendering for bfs_debug

--- crates/bfs_debug/src/lib.rs
+++ crates/bfs_debug/src/lib.rs
@@ -55,7 +55,7 @@ pub fn color_dirty_rects(map: Res<ParticleMap>, mut chunk_gizmos: Gizmos<DebugGi
 pub fn color_hibernating_chunks(map: Res<ParticleMap>, mut chunk_gizmos: Gizmos<DebugGizmos>) {
     map.iter_chunks().for_each(|chunk| {
         let rect = Rect::from_corners(chunk.region().min.as_vec2(), chunk.region().max.as_vec2());
-        if chunk.next_dirty_rect().is_none() {
+        if chunk.dirty_rect().is_none() {
             chunk_gizmos.rect_2d(
                 rect.center(),
                 rect.size() + Vec2::splat(1.),
@@ -66,7 +66,7 @@ pub fn color_hibernating_chunks(map: Res<ParticleMap>, mut chunk_gizmos: Gizmos<
 
     map.iter_chunks().for_each(|chunk| {
         let rect = Rect::from_corners(chunk.region().min.as_vec2(), chunk.region().max.as_vec2());
-        if chunk.next_dirty_rect().is_some() {
+        if chunk.dirty_rect().is_some() {
             chunk_gizmos.rect_2d(
                 rect.center(),
                 rect.size() + Vec2::splat(1.),
