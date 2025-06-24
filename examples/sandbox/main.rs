6860855d512cfa26706d7c9139e2842441a289a2
Jeff Barfield <jeffreybarfield@protonmail.com>
2025-06-24 01:44:50 -0400

    Add plugin in sandbox in a more ergonomic way

--- examples/sandbox/main.rs
+++ examples/sandbox/main.rs
@@ -6,7 +6,6 @@ mod ui;
 
 use std::time::Duration;
 
-use bfs_internal::debug::FallingSandDebugPlugin;
 use brush::*;
 use camera::*;
 use scenes::*;
@@ -15,33 +14,32 @@ use ui::*;
 
 use bevy::{prelude::*, window::WindowMode};
 use bevy_egui::EguiPlugin;
-use bevy_falling_sand::prelude::FallingSandPlugin;
+use bevy_falling_sand::prelude::{FallingSandDebugPlugin, FallingSandPlugin};
 
 fn main() {
-    let mut app = App::new();
-    app.add_plugins((
-        DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Falling Sandbox".into(),
-                mode: WindowMode::BorderlessFullscreen(MonitorSelection::Primary),
+    App::new()
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Falling Sandbox".into(),
+                    mode: WindowMode::BorderlessFullscreen(MonitorSelection::Primary),
+                    ..default()
+                }),
                 ..default()
             }),
-            ..default()
-        }),
-        EguiPlugin {
-            enable_multipass_for_primary_context: false,
-        },
-        FallingSandPlugin {
-            length_unit: 8.0,
-            spatial_refresh_frequency: Duration::from_millis(50),
-        },
-        FallingSandDebugPlugin,
-        CameraPlugin,
-        ParticleSetupPlugin,
-        BrushPlugin,
-        ScenesPlugin,
-        UIPlugin,
-    ))
-    .insert_resource(ClearColor(Color::srgba(0.17, 0.16, 0.15, 1.0)))
-    .run();
+            EguiPlugin {
+                enable_multipass_for_primary_context: false,
+            },
+            FallingSandPlugin::default()
+                .with_length_unit(8.0)
+                .with_spatial_refresh_frequency(Duration::from_millis(50)),
+            FallingSandDebugPlugin,
+            CameraPlugin,
+            ParticleSetupPlugin,
+            BrushPlugin,
+            ScenesPlugin,
+            UIPlugin,
+        ))
+        .insert_resource(ClearColor(Color::srgba(0.17, 0.16, 0.15, 1.0)))
+        .run();
 }
