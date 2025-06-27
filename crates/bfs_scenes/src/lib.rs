d09794eff4444c5b86ee3cdd232841194baf4501
noprobelm <noprobelm@protonmail.com>
2025-06-27 17:42:00 -0400

    Document bfs_scenes

--- crates/bfs_scenes/src/lib.rs
+++ crates/bfs_scenes/src/lib.rs
@@ -1,4 +1,13 @@
-mod events;
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![warn(
+    clippy::nursery,
+    clippy::pedantic,
+    nonstandard_style,
+    rustdoc::broken_intra_doc_links,
+    missing_docs
+)]
+#![allow(clippy::default_trait_access, clippy::module_name_repetitions)]
+//! Provides scene loading and saving functionality for the Falling Sand simulation.
 
 use bevy::prelude::*;
 use bfs_core::{Particle, ParticlePosition};
@@ -6,37 +15,46 @@ use ron::de::from_reader;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Write;
+use std::path::PathBuf;
 
-pub use events::*;
-
+/// Provides the constructs and systems necessary for saving and loading particle scenes in the
+/// Falling Sand simulation.
 pub struct FallingSandScenesPlugin;
 
 impl Plugin for FallingSandScenesPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(EventsPlugin);
-        app.add_systems(
-            Update,
-            save_scene_system.run_if(on_event::<crate::events::SaveSceneEvent>),
-        )
-        .add_systems(
-            Update,
-            load_scene_system.run_if(on_event::<crate::events::LoadSceneEvent>),
-        );
+        app.add_event::<LoadSceneEvent>()
+            .add_event::<SaveSceneEvent>()
+            .add_systems(Update, save_scene_system.run_if(on_event::<SaveSceneEvent>))
+            .add_systems(Update, load_scene_system.run_if(on_event::<LoadSceneEvent>));
     }
 }
 
+/// Particle type and position data for saving/loading scenes.
 #[derive(Serialize, Deserialize)]
 pub struct ParticleData {
+    /// The particle type.
     pub particle_type: Particle,
+    /// The particle position.
     pub position: ParticlePosition,
 }
 
+/// [`ParticleData`] wrapped in a scene for loading.
 #[derive(Serialize, Deserialize)]
 pub struct ParticleScene {
+    /// The particles the scene is composed of.
     pub particles: Vec<ParticleData>,
 }
 
-pub fn save_scene_system(
+/// Triggers systems to save the current particle scene to a file.
+#[derive(Event)]
+pub struct SaveSceneEvent(pub PathBuf);
+
+/// Triggers systems to load particles from a file.
+#[derive(Event)]
+pub struct LoadSceneEvent(pub PathBuf);
+
+fn save_scene_system(
     particle_query: Query<(&Particle, &ParticlePosition)>,
     mut ev_save_scene: EventReader<SaveSceneEvent>,
 ) {
@@ -57,7 +75,7 @@ pub fn save_scene_system(
     }
 }
 
-pub fn load_scene_system(mut commands: Commands, mut ev_load_scene: EventReader<LoadSceneEvent>) {
+fn load_scene_system(mut commands: Commands, mut ev_load_scene: EventReader<LoadSceneEvent>) {
     for ev in ev_load_scene.read() {
         let file = File::open(ev.0.clone()).expect("Failed to open RON file");
         let particle_scene: ParticleScene = from_reader(file).expect("Failed to load RON file");
