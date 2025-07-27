02b8071e690810fd41a09856ea1772149c08f630
noprobelm <noprobelm@protonmail.com>
2025-07-27 15:59:21 -0400

    Move reset chunks and clear particle type children to preupdate

--- crates/bfs_core/src/map.rs
+++ crates/bfs_core/src/map.rs
@@ -17,7 +17,7 @@ impl Plugin for ParticleMapPlugin {
             .add_event::<ClearParticleTypeChildrenEvent>()
             .add_systems(Startup, setup_particle_map)
             .add_systems(
-                PostUpdate,
+                PreUpdate,
                 (
                     reset_chunks.in_set(ParticleSimulationSet),
                     ev_clear_particle_type_children,
