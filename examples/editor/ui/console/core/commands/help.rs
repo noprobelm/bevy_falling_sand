93d1d059d4af4199e8915c36119ba4f2bcf642fa
Jeff Barfield <jeffreybarfield@protonmail.com>
2025-07-29 00:22:45 -0400

    Refactor help command

--- examples/editor/ui/console/core/commands/help.rs
+++ examples/editor/ui/console/core/commands/help.rs
@@ -1,15 +1,20 @@
 use bevy::prelude::*;
 
-use super::super::core::{
-    ConsoleCommand, ConsoleCommandEntered, ConsoleConfiguration, PrintConsoleLine,
-};
+use crate::ui::console::core::CommandNode;
+
+use super::super::core::{ConsoleCommand, ConsoleConfiguration, PrintConsoleLine};
 
 pub struct HelpCommandPlugin;
 
 impl Plugin for HelpCommandPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, help_command_system);
+        app.add_observer(on_show_help);
     }
+}
+
+#[derive(Event)]
+pub struct ShowHelpEvent {
+    pub target_command: Option<String>,
 }
 
 #[derive(Default)]
@@ -26,53 +31,50 @@ impl ConsoleCommand for HelpCommand {
 
     fn execute_action(
         &self,
-        _args: &[String],
+        args: &[String],
         _console_writer: &mut EventWriter<PrintConsoleLine>,
-        _commands: &mut Commands,
+        commands: &mut Commands,
     ) {
-        // The actual help logic is handled by the help_command_system
-        // This is empty because we need access to ConsoleConfiguration
+        let target_command = args.first().cloned();
+        commands.trigger(ShowHelpEvent { target_command });
     }
 }
 
-fn help_command_system(
-    mut cmd: EventReader<ConsoleCommandEntered>,
+fn on_show_help(
+    trigger: Trigger<ShowHelpEvent>,
     config: Res<ConsoleConfiguration>,
     mut writer: EventWriter<PrintConsoleLine>,
 ) {
-    for command_event in cmd.read() {
-        if command_event.command_path.len() >= 1 && command_event.command_path[0] == "help" {
-            if let Some(target_cmd) = command_event.args.first() {
-                if let Some(root_node) = config.command_tree.get(target_cmd) {
-                    show_command_tree_help(&root_node, vec![target_cmd.clone()], &mut writer);
-                } else {
-                    writer.write(PrintConsoleLine::new(format!(
-                        "Command '{}' does not exist",
-                        target_cmd
-                    )));
-                }
-            } else {
-                writer.write(PrintConsoleLine::new("Available commands:".to_string()));
+    let event = trigger.event();
 
-                for (name, node) in &config.command_tree {
-                    writer.write(PrintConsoleLine::new(format!(
-                        "  {} - {}",
-                        name, node.description
-                    )));
-                    if !node.children.is_empty() {
-                        writer.write(PrintConsoleLine::new(format!(
-                            "    (has subcommands: {})",
-                            node.children.keys().cloned().collect::<Vec<_>>().join(", ")
-                        )));
-                    }
-                }
+    if let Some(target_cmd) = &event.target_command {
+        if let Some(root_node) = config.command_tree.get(target_cmd) {
+            show_command_tree_help(root_node, vec![target_cmd.clone()], &mut writer);
+        } else {
+            writer.write(PrintConsoleLine::new(format!(
+                "Command '{target_cmd}' does not exist"
+            )));
+        }
+    } else {
+        writer.write(PrintConsoleLine::new("Available commands:".to_string()));
+
+        for (name, node) in &config.command_tree {
+            writer.write(PrintConsoleLine::new(format!(
+                "  {} - {}",
+                name, node.description
+            )));
+            if !node.children.is_empty() {
+                writer.write(PrintConsoleLine::new(format!(
+                    "    (has subcommands: {})",
+                    node.children.keys().cloned().collect::<Vec<_>>().join(", ")
+                )));
             }
         }
     }
 }
 
 fn show_command_tree_help(
-    node: &super::super::core::CommandNode,
+    node: &CommandNode,
     path: Vec<String>,
     writer: &mut EventWriter<PrintConsoleLine>,
 ) {
@@ -97,3 +99,4 @@ fn show_command_tree_help(
         }
     }
 }
+
